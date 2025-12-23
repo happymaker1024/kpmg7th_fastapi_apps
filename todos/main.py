d040d0a0f87d94878b49e5aa13e8d11c7286a364
happymaker <happymaker1024@gmail.com>
2025-12-24 04:40:10 +0900

    todo앱 완성

--- todos/main.py
+++ todos/main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI, Request
+from fastapi import Depends, FastAPI, Form, Request, status
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from sqlalchemy.orm import Session
 
+from database import engine, SessionLocal, Base
 import os
+
+import models
+
+# models에 정의한 모든 클래스, 연결한 DB엔진에 테이블로 생성
+Base.metadata.create_all(bind=engine)
 
 # FastAPI() 객체 생성
 app = FastAPI()
@@ -17,12 +25,54 @@ templates = Jinja2Templates(directory=f"{abs_path}/templates")
 # app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/static", StaticFiles(directory=f"{abs_path}/static"), name="static")
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        # 마지막에 무조건 닫음
+        db.close()
 
+# todo 조회
 @app.get("/")
-def home(request: Request):
-    todo = 200
-    title = "fastapi 기본 실습"
-    return templates.TemplateResponse("index.html",
-                                      {"request": request,
-                                       "todos": todo,
-                                       "title": title})
+async def home(request: Request, db: Session = Depends(get_db)):
+    todos = db.query(models.Todo).order_by(models.Todo.id.desc())
+    return templates.TemplateResponse("index.html", 
+                                      {"request": request, 
+                                       "todos": todos})
+# todo 추가
+@app.post("/add")
+async def add(request: Request, task: str = Form(...), db: Session = Depends(get_db)):
+	# todo 객체 만들기
+    todo = models.Todo(task=task)
+    # todo 추가하기
+    db.add(todo)
+    # db 적용하기
+    db.commit()
+    return RedirectResponse(url=app.url_path_for("home"), 
+                            status_code=status.HTTP_303_SEE_OTHER)
+
+# 수정을 위한 조회
+@app.get("/edit/{todo_id}")
+async def edit(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    return templates.TemplateResponse("edit.html", {"request": request, "todo": todo})
+
+# 수정한 것 적용
+@app.post("/edit/{todo_id}")
+async def update(request: Request, todo_id: int, task: str = Form(...), 
+                 completed: bool = Form(False), db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    todo.task = task
+    todo.completed = completed
+    db.commit()
+    return RedirectResponse(url=app.url_path_for("home"), 
+                            status_code=status.HTTP_303_SEE_OTHER)
+
+@app.get("/delete/{todo_id}")
+async def delete(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    db.delete(todo)
+    db.commit()
+    return RedirectResponse(url=app.url_path_for("home"), 
+                            status_code=status.HTTP_303_SEE_OTHER)
